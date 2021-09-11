6928dbc4eca420ba3b14f3bdd5ddc107ca6acab5
Jonathan Brant <jbrant1995@gmail.com>
2021-09-11 13:54:30 -0300

    add localStorage

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, Input } from '@angular/core';
 import { Todo } from '../models/ToDo';
 
+const LOCAL_STORAGE_KEY:string = "todolist";
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  tasks: Todo[];
+  private tasks: Todo[];
   constructor() { 
-    this.tasks = [
-    ]
+    this.tasks = this.loadLocalData();
   } 
   
   getTasks():Todo[] {
@@ -18,11 +19,22 @@ export class TaskService {
   addTasks(task:Todo) {
     this.tasks.push(task)
     console.log(this.tasks)
+    this.saveLocalData()
   }
 
   removeTasks(task:Todo) {
     console.log("clicou")
     this.tasks.splice(this.tasks.indexOf(task), 1)
+    this.saveLocalData()
+  }
+
+  public saveLocalData(): void {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.tasks));
+  }
+
+  private loadLocalData(): Todo[] {
+    const localData = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+    return localData ? JSON.parse(localData) : [];
   }
 
 }
